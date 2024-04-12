82b64fce7be01f168d219d074306137837115f1f
Kendall <kendall.kee@outlook.com>
2024-04-11 22:11:46 -0700

    updates from week 10

--- java/projects/ProjectsApp.java
+++ java/projects/ProjectsApp.java
@@ -13,10 +13,13 @@ import projects.service.ProjectService;
 public class ProjectsApp {
 	private Scanner scanner = new Scanner(System.in);
 	private ProjectService projectService = new ProjectService(); 
+	private Project curProject;
 		
 		//@formatter:off
 		private List<String> operations = List.of(
-				"1) Add a project."
+				"1) Add a project.",
+				"2) List projects.",
+				"3) Select a project."
 				);
 		//@formatter:on
 		
@@ -39,6 +42,12 @@ public class ProjectsApp {
 					case 1:
 						createProject();
 						break;
+					case 2:
+						listProjects();
+						break;
+					case 3:
+						selectProject();
+						break;
 					
 					default:
 						System.out.println("\n" + selection + " is not a valid selection. Try again.");
@@ -47,10 +56,28 @@ public class ProjectsApp {
 				}
 				catch(Exception e) {
 					System.out.println("\nError: " + e + " Try again.");
+					e.printStackTrace();
 				}
 			}
 		}
 	
+	//Method to select the desired project
+	private void selectProject() {
+			listProjects();
+			Integer projectId = getIntInput("Enter a project ID to select a project.");
+			//used to un-select current selected project
+			curProject = null;
+			
+			curProject = projectService.fetchProjectById(projectId);
+			
+		}
+	//Method to list the projects from the database
+	private void listProjects() {
+			List <Project> projects = projectService.fetchAllProjects();
+			System.out.println("\nProjects");
+			projects.forEach(project -> System.out.println("   " + project.getProjectId() + ": " + project.getProjectName()));
+			
+		}
 	//Method get project details from the user
 	private void createProject() {
 			String projectName = getStringInput("Enter the project name.");
@@ -123,6 +150,12 @@ public class ProjectsApp {
 		System.out.println("\nThese are the available selections. Press the Enter ket to quit:");
 		operations.forEach(line -> System.out.println("   " + line));
 		
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project.");
+		}
+		else {
+			System.out.println("\nYou are working with a project.");
+		}
 	}
 
 }
--- java/projects/service/ProjectService.java
+++ java/projects/service/ProjectService.java
@@ -1,5 +1,8 @@
 package projects.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import projects.dao.ProjectDao;
 import projects.entity.Project;
 
@@ -11,4 +14,17 @@ public class ProjectService {
 		return projectDao.insertProject(project);
 	}
 
+	//Method calls the project DAO to get all project rows without the details
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects();
+	}
+
+	public Project fetchProjectById(Integer projectId) {	
+		//temp assign variable type 
+				//Optional<Project> op = projectDao.fetchProjectById(projectId);
+				//may have to delete the top one
+		return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException("Project with project ID=" + projectId + " does not exist"));
+	}
 }
+
+
